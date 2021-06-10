4044f5aedd79c7a1d94458959c7b0e133d18cbda
habout <hossain.aboutalebi@gmail.com>
2021-06-09 21:48:12 -0400

    First commit

--- data_loader/data_extractor.py
+++ data_loader/data_extractor.py
@@ -2,18 +2,45 @@ import numpy as np
 
 class Data_extractor:
 
-    def __init__(self,file_trj,file_end):
-        pass
+    def __init__(self,trj_len=10,action_shape=3,max_num_trj=100000):
+        self.action_shape=action_shape
+        self.trj_len=trj_len
+        self.max_num_trj=max_num_trj
 
-    @staticmethod
-    def extract(file_trj,file_end):
+    def update_trj_len(self,trj_len):
+        self.trj_len=trj_len
+
+    def extract(self,file_trj,file_end):
         trj=np.load(file_trj)
         end=np.load(file_end)
-        final_list= Data_extractor.create_action_trj(trj,end)
+        final_list= self.create_action_trj(trj,end)
+        return final_list
 
-    @staticmethod
-    def create_action_trj(trj,end):
-        pass
+    def create_action_trj(self,trj,end):
+        final_list=[]
+        index = 0
+        for i in range(len(end)):
+            if(i+1>self.max_num_trj):
+                continue
+            trajectory=[]
+            for j in range(index,end[i]+1):
+                if(len(trajectory)>=self.trj_len):
+                    continue
+                trajectory.append(trj[j])
+            if(len(trajectory)<self.trj_len):
+                self.pad_trajectory(trajectory)
+            index=end[i]+1
+            final_list.append(np.array(trajectory))
+        return np.array(final_list)
+
+    def pad_trajectory(self,trajectory):
+        dif=self.trj_len-len(trajectory)
+        for i in range(dif):
+            trajectory.append(np.zeros(self.action_shape))
+
+    def create_dataset_TCN_ch(self, trj1, trj2):
+        return np.concatenate((trj1,trj2),axis=len(trj1.shape)-1)
+
 
 
 
--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-
+from data_loader.data_extractor import Data_extractor
 
 if __name__ == '__main__':
-    shadow_trj_ddpg = "/Users/hosseinaboutalebi/Downloads/PrivAttack-Data/shadow/seed_5/DDPG_Robust_Hopper-v2_20_5_action.npy"
-    shadow_end_ddpg = "/Users/hosseinaboutalebi/Downloads/PrivAttack-Data/shadow/seed_5/DDPG_Robust_Hopper-v2_20_5_trajectory_end_index.npy"
-
+    de = Data_extractor(trj_len=10, action_shape=3, max_num_trj=100000)
+    shadow_trj_ddpg_f = "/home/hossein.aboutalebi/data/PrivAttack-Data/shadow/seed_5/DDPG_Robust_Hopper-v2_20_5_action.npy"
+    shadow_end_ddpg_f = "/home/hossein.aboutalebi/data/PrivAttack-Data/shadow/seed_5/DDPG_Robust_Hopper-v2_20_5_trajectory_end_index.npy"
+    shadow_trj_ddpg = de.extract(shadow_trj_ddpg_f, shadow_end_ddpg_f)
+    shadow_trj_bcq_f = "/home/hossein.aboutalebi/data/PrivAttack-Data/shadow/seed_5/BCQ_target_Robust_Hopper-v2_20_5_1000000_compatible_action.npy"
+    shadow_end_bcq_f = "/home/hossein.aboutalebi/data/PrivAttack-Data/shadow/seed_5/BCQ_target_Robust_Hopper-v2_20_5_1000000_compatible_trajectory_end_index.npy"
+    shadow_trj_bcq = de.extract(shadow_trj_ddpg_f, shadow_end_ddpg_f)
+    train_dataset = de.create_dataset_TCN_ch(shadow_trj_ddpg, shadow_trj_bcq)
+    print("yes")
