de385410f7b2d64f5fc61e1ea60ffef8c078d580
habout <hossain.aboutalebi@gmail.com>
2021-06-11 17:22:54 -0400

    Added Argparser

--- main.py
+++ main.py
@@ -2,16 +2,101 @@ from data_loader.balanced_sampler import BalancedBatchSampler
 from data_loader.data_extractor import Data_extractor
 from torch.utils.data import DataLoader
 
+from tcn import TCN
 from trainer.tcn_train import TCNTrainer
+from utils.file_manager import File_Manager
 from utils.files import get_trj_end_npy
 from data_loader.dataset import TCNDataset
 import numpy as np
+import datetime
+import argparse
+import os
+import random
 import torch
+import logging
+logger = logging.getLogger(__name__)
+
+parser = argparse.ArgumentParser(description='TCN for Privacy Adversarial Attack')
+
+# *********************************** General Setting ********************************************
+parser.add_argument('-o', '--output_path', default=os.path.expanduser('~') + '/results_privacy',
+                    help='output path for files produced by the agent')
+parser.add_argument('--cuda_n', type=str, default="0", help='random seed (default: 4)')
+parser.add_argument('--seed', type=int, default=1111, help='random seed (default: 1111)')
+
+# *********************************** Dataset Loading Setting ********************************************
+parser.add_argument('--trj_len', type=int, default=10,
+                    help='trajectory length (default: 10)')
+parser.add_argument('--n_output', type=int, default=2,
+                    help='number of hidden units per layer (default: 600)')
+parser.add_argument('--batch_size', type=int, default=16, metavar='N',
+                    help='batch size (default: 16)')
+parser.add_argument('--num_workers', type=int, default=4,
+                    help='number of workers for torchvision Dataloader')
+
+# *********************************** Model Setting ********************************************
+parser.add_argument('--dropout', type=float, default=0.45,
+                    help='dropout applied to layers (default: 0.45)')
+parser.add_argument('--ksize', type=int, default=3,
+                    help='kernel size (default: 3)')
+parser.add_argument('--nhid', type=int, default=600,
+                    help='number of hidden units per layer (default: 600)')
+parser.add_argument('--levels', type=int, default=4,
+                    help='# of levels (default: 4)')
+
+# *********************************** Training Setting ********************************************
+parser.add_argument('--clip', type=float, default=0.35,
+                    help='gradient clip, -1 means no clip (default: 0.35)')
+parser.add_argument('--epochs', type=int, default=100,
+                    help='upper epoch limit (default: 100)')
+parser.add_argument('--lr', type=float, default=4,
+                    help='initial learning rate (default: 4)')
+parser.add_argument('--optim', type=str, default='SGD',
+                    help='optimizer type (default: SGD)')
+args = parser.parse_args()
+
+# sets the seed for making it comparable with other implementations
+torch.manual_seed(args.seed)
+torch.cuda.manual_seed(args.seed)
+np.random.seed(args.seed)
+random.seed(args.seed)
+
+# *********************************** Logging Config ********************************************
+current_time = (str(datetime.datetime.now()).replace(" ", "#")).replace(":", "-")
+output_path=args.output_path
+file_path_results = output_path + "/" + current_time
+File_Manager.set_file_path(file_path_results)
+if not os.path.exists(output_path):
+    os.makedirs(output_path)
+os.mkdir(file_path_results)
+logging.basicConfig(level=logging.DEBUG, filename=file_path_results + "/log.txt")
+logging.getLogger().addHandler(logging.StreamHandler())
+
+header = "===================== Experiment configuration ========================"
+logger.info(header)
+args_keys = list(vars(args).keys())
+args_keys.sort()
+max_k = len(max(args_keys, key=lambda x: len(x)))
+for k in args_keys:
+    s = k + '.' * (max_k - len(k)) + ': %s' % repr(getattr(args, k))
+    logger.info(s + ' ' * max((len(header) - len(s), 0)))
+logger.info("=" * len(header))
+
+# *********************************** Environment Building ********************************************
+device = torch.device("cuda:" + args.cuda_n if True else "cpu")
+logger.info("device is set for: {}".format(device))
+
+
+
 
 if __name__ == '__main__':
-    batch_size = 16
-    epochs=10
-    de = Data_extractor(trj_len=10, action_shape=3, max_num_trj=100000)
+
+    num_chans = [args.nhid] * (args.levels - 1) + [args.emsize]
+    batch_size = args.batch_size
+    epochs=args.epochs
+    k_size = args.ksize
+    dropout = args.dropout
+    de = Data_extractor(trj_len=args.trj_len, action_shape=3, max_num_trj=100000)
     shadow_trj_ddpg_fp, shadow_end_ddpg_fp = get_trj_end_npy(
         "/home/hossein.aboutalebi/data/PrivAttack-Data/shadow/seed_5/DDPG_Robust_Hopper-v2_20_5")
     shadow_trj_ddpg_p = de.extract(shadow_trj_ddpg_fp, shadow_end_ddpg_fp)
@@ -27,5 +112,9 @@ if __name__ == '__main__':
     sampler = torch.utils.data.sampler.WeightedRandomSampler(torch.tensor([1, 1]), 2)
     train_loader = DataLoader(train_dataset, batch_size=batch_size, num_workers=4,
                               sampler=BalancedBatchSampler(train_dataset))
+
+    #loading the model
+    model = TCN(args.trj_len, args.n_output, num_chans, dropout=dropout, kernel_size=k_size)
+
     trainer=TCNTrainer(train_loader)
     trainer.run(epochs)
--- tcn.py
+++ tcn.py
@@ -45,6 +45,7 @@ class TemporalBlock(nn.Module):
         return self.relu(out + res)
 
 
+
 class TemporalConvNet(nn.Module):
     def __init__(self, num_inputs, num_channels, kernel_size=2, dropout=0.2):
         super(TemporalConvNet, self).__init__()
@@ -61,3 +62,25 @@ class TemporalConvNet(nn.Module):
 
     def forward(self, x):
         return self.network(x)
+
+
+class TCN(nn.Module):
+
+    def __init__(self, input_size, output_size, num_channels,
+                 kernel_size=2, dropout=0.3):
+        super(TCN, self).__init__()
+        self.tcn = TemporalConvNet(input_size, num_channels, kernel_size, dropout=dropout)
+
+        self.decoder = nn.Linear(num_channels[-1], output_size)
+        self.init_weights()
+
+    def init_weights(self):
+        self.decoder.bias.data.fill_(0)
+        self.decoder.weight.data.normal_(0, 0.01)
+
+    def forward(self, input):
+        """Input ought to have dimension (N, C_in, L_in), where L_in is the seq_len; here the input is (N, L, C)"""
+        emb = self.drop(self.encoder(input))
+        y = self.tcn(emb.transpose(1, 2)).transpose(1, 2)
+        y = self.decoder(y)
+        return y.contiguous()
