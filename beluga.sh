36eaa9a85e94f5f308cb8428c5fe85bbf962e53a
habout <hossain.aboutalebi@gmail.com>
2021-07-05 20:17:23 -0400

    Fixed errors

--- beluga.sh
+++ beluga.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-trj_len="100"
+trj_len="200"
 seed_number="0"
 seed_shadow1='seed_5'
-url_seed_shadow1='~/projects/rrg-dprecup/samin/learning_output/Hopper-v2/1000000/4000000/20/5/100/buffers/*'
+url_seed_shadow1='~/projects/rrg-dprecup/samin/learning_output/Hopper-v2/1000000/4000000/20/5/200/buffers/*'
 seed_shadow2='seed_100'
-url_seed_shadow2='~/projects/rrg-dprecup/samin/learning_output/Hopper-v2/1000000/4000000/20/100/100/buffers/*'
+url_seed_shadow2='~/projects/rrg-dprecup/samin/learning_output/Hopper-v2/1000000/4000000/20/100/200/buffers/*'
 seed_target1='seed_75'
-url_seed_seed_target1='~/projects/rrg-dprecup/samin/learning_output/Hopper-v2/1000000/4000000/200/75/100/buffers/*'
+url_seed_seed_target1='~/projects/rrg-dprecup/samin/learning_output/Hopper-v2/1000000/4000000/200/75/200/buffers/*'
 seed_target2='seed_700'
-url_seed_seed_target2='~/projects/rrg-dprecup/samin/learning_output/Hopper-v2/1000000/4000000/200/700/100/buffers/*'
+url_seed_seed_target2='~/projects/rrg-dprecup/samin/learning_output/Hopper-v2/1000000/4000000/200/700/200/buffers/*'
 
 cd /home/hossein.aboutalebi/data/PrivAttack-Data
 
