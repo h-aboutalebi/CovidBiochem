90ceccd1f2580fff5e3f29057d17ba8943652952
Hossein Aboutalebi <hossain.aboutalebi@gmail.com>
2022-02-14 02:04:24 -0500

    set as seed

---
+++ Covid_biochem/utility/utils.py
@@ -0,0 +1,16 @@
+import random
+import numpy as np
+import os
+import torch
+
+def seed_everything(seed=42):
+    """"
+    Seed everything.
+    """   
+    random.seed(seed)
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.deterministic = True
