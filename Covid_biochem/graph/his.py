af9dde9dcfa785422673b6cf15294a224086343d
hosseinaboutalebi <hossain.aboutalebi@gmail.com>
2022-04-01 15:42:32 +0430

    Changed graph format 2

--- Covid_biochem/graph/his.py
+++ Covid_biochem/graph/his.py
@@ -1,19 +1,7 @@
 import matplotlib.pyplot as plt
 
 
-def create_his(df, target):
-    count_dict=dict(df[target].value_counts())
-    labels, counts=[],[]
-    for key in count_dict.keys():
-        labels.append(key)
-        counts.append(count_dict[key])
-    fig, ax = plt.subplots()
-    plt.ylim([0, max(counts)+0.2*max(counts)])
-    labels.append("None")
-    plt.title(target)
-    plt.ylabel("count")
-    counts.append(df[target].isna().sum())
-    bars = ax.bar(labels, counts)
+def  hist_font(ax,bars):
     # Axis formatting.
     ax.spines['top'].set_visible(False)
     ax.spines['right'].set_visible(False)
@@ -24,22 +12,38 @@ def create_his(df, target):
     ax.yaxis.grid(True, color='#EEEEEE')
     ax.xaxis.grid(False)
 
-    # Grab the color of the bars so we can make the
-    # text the same color.
-    bar_color = bars[0].get_facecolor()
-
     # Add text annotations to the top of the bars.
-    # Note, you'll have to adjust this slightly (the 0.3)
-    # with different data.
+    bar_color = bars[0].get_facecolor()
     for bar in bars:
         ax.text(
             bar.get_x() + bar.get_width() / 2,
-            bar.get_height() + 0.3,
+            bar.get_height()+ 50,
             round(bar.get_height(), 1),
             horizontalalignment='center',
             color=bar_color,
             weight='bold'
         )
+
+
+def create_his(df, target):
+    count_dict=dict(df[target].value_counts())
+    labels, counts=[],[]
+    for key in count_dict.keys():
+        labels.append(key)
+        counts.append(count_dict[key])
+    fig, ax = plt.subplots()
+    plt.ylim([0, max(counts)+0.1*max(counts)])
+    labels.append("None")
+    # plt.title(target)
+    counts.append(df[target].isna().sum())
+    bars = ax.bar(labels, counts)
+    hist_font(ax, bars)
+
+    # Add labels and a title.
+    ax.set_xlabel('Classes', labelpad=15, color='#333333')
+    ax.set_ylabel("Count", labelpad=15, color='#333333')
+    ax.set_title(target, pad=15, color='#333333',
+                 weight='bold')
 
     fig.tight_layout()
     plt.show()
